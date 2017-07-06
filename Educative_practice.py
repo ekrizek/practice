70862f95671ccb5fa3132c34bcf3a94dbf12b447
elliottkrizek <john.krizek@ties.k12.mn.us>
2017-07-06 09:39:45 -0500

    hope this works

--- Educative_practice.py
+++ Educative_practice.py
@@ -1,14 +1,14 @@
 import csv
 
-def csv_reader(file_obj):
+def csv_dict_reader(file_obj):
     """
-    Read a csv file
+    Read a CSV file using csv.DictReader
     """
-    reader = csv.reader(file_obj)
-    for row in reader:
-        print(" ".join(row))
+    reader = csv.DictReader(file_obj, delimiter=',')
+    for line in reader:
+        print(line["first_name"]),
+        print(line["last_name"])
 
 if __name__ == "__main__":
-    csv_path = "data.csv"
-    with open(csv_path, "r") as f_obj:
-        csv_reader(f_obj)
+    with open("data.csv") as f_obj:
+        csv_dict_reader(f_obj)
