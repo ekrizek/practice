12ee0150239f0a849c5b18f99d2c897ebcbb4db2
elliottkrizek <john.krizek@ties.k12.mn.us>
2017-07-06 10:51:17 -0500

    see if this works

--- Educative_practice.py
+++ Educative_practice.py
@@ -8,7 +8,3 @@ def csv_dict_reader(file_obj):
     for line in reader:
         print(line["first_name"]),
         print(line["last_name"])
-
-if __name__ == "__main__":
-    with open("data.csv") as f_obj:
-        csv_dict_reader(f_obj)
