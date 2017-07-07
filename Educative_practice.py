216228c64840b6305a316e838246fefd96dcab7c
ElliottKrizek <john.krizek@ties.k12.mn.us>
2017-07-07 06:44:44 -0500

    see if this works

--- Educative_practice.py
+++ Educative_practice.py
@@ -7,8 +7,10 @@ def csv_dict_reader(file_obj):
     reader = csv.DictReader(file_obj, delimiter=',')
     for line in reader:
         print(line["siteId"]),
-        print(line["studentId"])
+        print(line["studentId"]),
+        print(line["date"])
 
 if __name__ == "__main__":
     with open("ORF-CES-W.csv") as f_obj:
         csv_dict_reader(f_obj)
+
